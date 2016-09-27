006e29aa4ff32520010158ee89d8fbebce42ac2b
JustinChou <justinchou186@gmail.com>
2016-09-27 14:27:13 +0800

    initialize project

---
+++ GradualLoading/GradualLoading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradualLoading
+//
+//  Created by JustinChou on 16/9/27.
+//  Copyright © 2016年 JustinChou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
